714233039fa91fc2e2b1e70b72813bb95154682f
kxf89910110 <1052831447@qq.com>
2019-04-17 06:43:47 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png"></img>
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    Practice hard, daily progress
+  </a>
+
+  <div class="float-right">
+    <a href="/about">About</a>
+  </div>
+</footer>
