94c267bb24f08e4b9da310cb92dfe512c74a214d
kxf89910110 <1052831447@qq.com>
2019-04-17 07:57:41 +0000

    Sign up Table

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,37 @@
 @section('title', 'Sign up')
 
 @section('content')
-<h1>Sign up</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>Sign Up</h5>
+    </div>
+
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        </div>
+
+        <div class="from-group">
+          <label for="email">Email:</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">Confirm Password:</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Sign Up</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
