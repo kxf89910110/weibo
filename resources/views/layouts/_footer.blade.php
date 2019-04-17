7eb703bc754f477ee2c9a5d8beb74b93d8b7f8d8
kxf89910110 <1052831447@qq.com>
2019-04-17 06:50:13 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
   </a>
 
   <div class="float-right">
-    <a href="/about">About</a>
+    <a href="{{ route('about') }}">About</a>
   </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+      <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
       <li class="nav-item"><a class="nav-link" href="#">Sign in</a></li>
     </ul>
   </div>
