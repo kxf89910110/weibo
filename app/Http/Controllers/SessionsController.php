b76828f43c2a0c0298f3fcba30f1ed6349c22fc2
kxf89910110 <1052831447@qq.com>
2019-04-17 09:45:39 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,7 +29,8 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', 'Welcome back!');
-           return redirect()->route('users.show', [Auth::user()]);
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
        } else {
            session()->flash('danger', 'Sorry, your email and password do not match');
            return redirect()->back()->withInput();
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            session()->flash('info', 'You are logged in and do not need to do it again.');
+            return redirect('/');
         }
 
         return $next($request);
